1d7fbae2ccb19b7848dacccce8d82a7da7874e6c
kohei izumi <unitedbz0401@gmail.com>
2020-03-07 17:28:49 +0900

    add test code

--- src/app/component/page/main/main.component.spec.ts
+++ src/app/component/page/main/main.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { MainComponent } from './main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from 'src/app/component/page/main/main.component';
+import { MaterialModule } from 'src/app/module/material.module';
+import { PipeModule } from 'src/app/module/pipe.module';
+import { SharedModule } from 'src/app/module/shared.module';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -8,7 +11,15 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [
+        MainComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule,
+        PipeModule,
+        SharedModule,
+      ],
     })
     .compileComponents();
   }));
@@ -22,4 +33,12 @@ describe('MainComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', () => {
+    fixture = TestBed.createComponent(MainComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('悲観的な天気予報');
+  });
+
 });
